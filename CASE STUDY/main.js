a0a931b101fa431874fe19ec7df31d202536bc9b
6thanh <sauthanh106@gmail.com>
2021-11-04 20:13:08 +0700

    modified case study

---
+++ CASE STUDY/main.js
@@ -0,0 +1,142 @@
+let bar = new Bar();
+let ball = new Ball(10);
+let isPlay = false;
+let speedBall = 0;
+let speedInput = 0;
+let interval;
+let time = 0;
+let countTime;
+
+startGame.addEventListener('click', function () {
+    startGame.innerText = 'START';
+    playGame.innerText = "Chọn tốc độ bóng, sau đó nhấn START để bắt đầu";
+    speed.style.display = 'block';
+});
+startGame.addEventListener('click', function () {
+    if (chooseSpeed()) {
+        playGameStart()
+    } else speed.value = '';
+})
+
+function playGameStart() {
+    startGame.style.display = 'none';
+    speed.style.display = 'none';
+    playGame.innerText = "";
+    start();
+}
+
+function chooseSpeed() {
+    speedInput = speed.value;
+    speedBall = parseFloat(speedInput);
+    if ((speedBall != speedInput || speedBall < 50 ||
+        speedBall > 100 || speedBall == null)) {
+        speed.value = '';
+    } else return true
+}
+
+function btnRestart() {
+    startGame.innerText = 'TIẾP TỤC'
+    startGame.style.display = 'block';
+    playGame.innerText = "Nhấn TIẾP TỤC để chơi lại";
+    startGame.addEventListener('click', function () {
+        location.reload();
+    })
+}
+
+function start() {
+    bar.drawBar();
+    ball.drawBall();
+    audio.innerHTML = '<audio loop autoplay source src="audio/Game-Boom-online.mp3" type="audio/mp3"></audio>'
+
+
+    function update() {
+        isPlay = checkPlay()
+        if (isPlay) {
+            ball.moveBall();
+            ball.checkCollision();
+            ball.drawBall()
+        } else pause()
+    }
+
+    function displayScores() {
+        time++
+        speedBall--
+        scores.innerHTML = time;
+    }
+    play()
+
+    function play() {
+        isPlay = true;
+        countTime = setInterval(displayScores, 1000)
+        interval = setInterval(update, speedBall);
+        audio.innerHTML = '<audio loop autoplay source src="audio/Game-Boom-online.mp3" type="audio/mp3"></audio>'
+    }
+
+    function pause() {
+        isPlay = false;
+        clearInterval(interval);
+        clearInterval(countTime);
+        audio.innerHTML = '';
+    }
+
+    function checkPlay() {
+        if (ball.distanceX == 0 && ball.distanceX == 0) {
+            setTimeout(endGame, 500);
+            return false
+        } else return true
+    }
+
+    function endGame() {
+        console.log(isPlay)
+        myCanvas.innerHTML = audio.innerHTML = '<audio autoplay source src="audio/Mario.mp3" type="audio/mp3"></audio>'
+        ctx.beginPath();
+        ctx.fillStyle = "#FFDF91"
+        ctx.fillRect(150, 80, 210, 220);
+        ctx.fillStyle = "blue";
+        ctx.font = '25px serif';
+        ctx.fillText("KẾT THÚC!", widthCanvas / 2 - 60, heightCanvas / 2 - 120);
+        ctx.fillStyle = "blue";
+        ctx.font = '20px serif';
+        ctx.fillText("Điểm của bạn là: " + time, widthCanvas / 2 - 80, heightCanvas / 2 - 80);
+        ctx.fillText("Tốc độ quả bóng là: " + (speedBall + time * 2), widthCanvas / 2 - 85, heightCanvas / 2 - 50);
+        ctx.closePath();
+        ctx.fill();
+        btnRestart();
+    }
+
+    window.addEventListener("keydown", function (keydown) {
+        if (isPlay) {
+            switch (keydown.key) {
+                case 'ArrowUp':
+                case 'ArrowDown':
+                case 'w':
+                case 'S':
+                    pause();
+                    break;
+                case 'ArrowRight':
+                case 'D':
+                    if (bar.barX + bar.widthBar + bar.distanceBar <= widthCanvas) {
+                        bar.moveRight();
+                        bar.drawBar();
+                    }
+                    break;
+                case 'ArrowLeft':
+                case 'A':
+                    if (bar.barX - bar.distanceBar >= 0) {
+                        bar.moveLeft();
+                        bar.drawBar();
+                    }
+                    break;
+            }
+        } else {
+            switch (keydown.key) {
+                case 'ArrowUp':
+                case 'ArrowDown':
+                case 'W':
+                case 'S':
+                    play();
+                    break;
+            }
+        }
+    });
+}
---
+++ CASE STUDY/obj.js
@@ -0,0 +1,84 @@
+let myCanvas = document.getElementById('myCanvas');
+let widthCanvas = parseFloat(myCanvas.getAttribute('width'));
+let heightCanvas = parseFloat(myCanvas.getAttribute('height'));
+let ctx = myCanvas.getContext('2d');
+let scores = document.getElementById('scores');
+let startGame = document.getElementById('startGame');
+let playGame = document.getElementById('playGame');
+let audio = document.getElementById('audio');
+let speed = document.getElementById('speed')
+
+class Bar {
+    constructor() {
+        this.barX = widthCanvas / 2 - 50;
+        this.barY = heightCanvas - 70;
+        this.distanceBar = 10;
+        this.widthBar = 100;
+        this.heightBar = 10;
+    }
+    drawBar() {
+        ctx.beginPath();
+        ctx.fillStyle = "#FFDF91";
+        ctx.fillRect(this.barX, this.barY, this.widthBar, this.heightBar);
+    }
+
+    moveRight() {
+        ctx.clearRect(this.barX, this.barY, this.widthBar, this.heightBar);
+        return this.barX += this.distanceBar;
+    }
+    moveLeft() {
+        ctx.clearRect(this.barX, this.barY, this.widthBar, this.heightBar);
+        return this.barX -= this.distanceBar;
+    }
+}
+
+class Ball {
+    constructor(distance) {
+        this.distanceX = distance;
+        this.distanceY = -distance;
+        this.radius = 10;
+        this.ballX = bar.barX + bar.widthBar / 2;
+        this.ballY = bar.barY - this.radius;
+    }
+
+    drawBall() {
+        ctx.beginPath();
+        ctx.arc(this.ballX, this.ballY, this.radius, 0, 2 * Math.PI, true);
+        ctx.fillStyle = "#EAAC7F";
+        ctx.closePath();
+        ctx.fill();
+    }
+    moveBall() {
+        ctx.beginPath();
+        ctx.strokeStyle = "#493323";
+        ctx.arc(this.ballX, this.ballY, this.radius, 0, 2 * Math.PI, true);
+        ctx.stroke();
+        ctx.fillStyle = "#493323";
+        ctx.closePath();
+        ctx.fill();
+        this.ballX += this.distanceX;
+        this.ballY += this.distanceY;
+        this.ballTop = this.ballY - this.radius;
+        this.ballRight = this.ballX + this.radius;
+        this.ballBottom = this.ballY + this.radius;
+        this.ballLeft = this.ballX - this.radius;
+    }
+    checkCollision() {
+        if (this.ballLeft <= 0 || this.ballRight >= widthCanvas) {
+            return this.distanceX = -this.distanceX;
+        }
+        if (this.ballTop <= 0) {
+            return this.distanceY = -this.distanceY
+        }
+        if (this.ballBottom >= bar.barY) {
+            if (this.ballX >= bar.barX &&
+                this.ballX <= bar.barX + bar.widthBar) {
+                return this.distanceY = -this.distanceY
+            }
+        }
+        if (this.ballBottom >= heightCanvas) {
+            this.distanceY = 0;
+            this.distanceX = 0;
+        }
+    }
+}
